bdbd67621eb57d3c648896b31bd9a8e511c70922
Ronen Lapushner <ronenk17@gmail.com>
2018-10-12 14:56:12 +0300

    Refactored name sanitation

--- nautilus-copypath.py
+++ nautilus-copypath.py
@@ -17,12 +17,16 @@ class CopyPathExtension(GObject.GObject, Nautilus.MenuProvider):
         # Initialize clipboard
         self.clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
 
+    def __sanitize_path(self, path):
+        # Replace actual spaces with Linux-compatible spaces
+        return path.replace(' ', '\\ ')
+
     def __copy_files_path(self, menu, files):
         pathstr = None
 
         # Get the paths for all the files.
         # Also, strip any protocol headers, if required.
-        paths = [fileinfo.get_location().get_path().replace(' ', '\\ ')
+        paths = [self.__sanitize_path(fileinfo.get_location().get_path())
                 for fileinfo in files]
         
         # Append to the path string
@@ -37,7 +41,8 @@ class CopyPathExtension(GObject.GObject, Nautilus.MenuProvider):
 
     def __copy_dir_path(self, menu, path):
         if path is not None:
-            self.clipboard.set_text(path.get_location().get_path(), -1)
+            pathstr = self.__sanitize_path(path.get_location().get_path())
+            self.clipboard.set_text(pathstr, -1)
 
     def get_file_items(self, window, files):
         # If there are many items to copy, change the label
